ee9f5e2a47d6ca66f357bcbfc260d291d40223dd
Will Anderson <williamv.andeson@gmail.com>
2013-03-09 17:18:59 -0500

    moved the delete button to the product show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,10 +73,10 @@ class ProductsController < ApplicationController
   # DELETE /products/1.json
   def destroy
     @product = Product.find(params[:id])
-    @product.destroy
+    @product.destroy 
 
     respond_to do |format|
-      format.html { redirect_to products_url }
+      format.html { redirect_to products_path }
       format.json { head :no_content }
     end
   end

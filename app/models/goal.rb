171d155f812c5a77dcadb0182ad73f33b292ca83
Will Anderson <williamv.anderson@gmail.com>
2013-05-19 13:00:27 -0400

    reformatted goal validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,8 @@
 class Goal < ActiveRecord::Base
   attr_accessible :goal, :product_id, :size, :completion_percentage
   belongs_to :product
-  validates :goal, :presence => true
+  
+  validates :goal,:presence => true
+  validates :size,:presence => true 
+  validates :completion_percentage,:presence => true 
 end

458a981b5f7cc481cbd1a145427a8309370e8b47
Will Anderson <williamv.anderson@gmail.com>
2013-05-26 15:56:40 -0400

    added test database config, product model validations, and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :overview
   has_many :goals, :dependent => :destroy
+  validates :name, :presence => true
+  validates :overview, :presence => true
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save product without name" do
+     product = Product.new
+     assert !product.save, "Saved the product without a name"
+   end
+
+  test "should not save product without product overview" do
+     product = Product.new
+     assert !product.save, "Saved the product without a product overview"
+   end
+
+
 end
+
+
